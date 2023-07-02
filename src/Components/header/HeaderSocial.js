5b779badce823707d973694e5a49f108e0482ee7
Vishnu-Sudharsan7 <vishnusudharsan.a@gmail.com>
2023-07-02 17:13:12 +0530

    bugs

--- src/Components/header/HeaderSocial.js
+++ src/Components/header/HeaderSocial.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import {AiFillLinkedin} from 'react-icons/ai';
-import{AiFillGithub} from 'react-icons/ai';
-import {AiOutlineDribbble} from 'react-icons/ai';
-
+import React, { useState, useEffect } from 'react';
+import { AiFillLinkedin, AiFillGithub, AiOutlineDribbble } from 'react-icons/ai';
 
 const HeaderSocial = () => {
-  return (
-    <div className='header__socials'>
-      <a href='https://linkedin.com' target='blank'><AiFillLinkedin/></a>
-      <a href='https://github.com' target='blank'><AiFillGithub/></a>
-      <a href='https://dribble.com' target='blank'><AiOutlineDribbble/></a>
-    </div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default HeaderSocial
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/data');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div className='header__socials' key={item._id}>
+          <a href={item.linkedin} target='_blank' rel='noopener noreferrer'>
+            <AiFillLinkedin />
+          </a>
+          <a href={item.git_link} target='_blank' rel='noopener noreferrer'>
+            <AiFillGithub />
+          </a>
+          <a href='' target='_blank' rel='noopener noreferrer'>
+            <AiOutlineDribbble />
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HeaderSocial;
